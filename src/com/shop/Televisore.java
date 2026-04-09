991cee43f73202e1dc39caedc144efe75c6540ed
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-09 20:23:53 +0200

    implementati getter e setter per prodotto televisore

--- src/com/shop/Televisore.java
+++ src/com/shop/Televisore.java
@@ -1,9 +1,41 @@
 package com.shop;
 
+import java.math.BigDecimal;
+
 public class Televisore extends Prodotto {
-    @
+    private float larghezza;
+    private float lunghezza;
+    private boolean isSmartTv;
 
-    public Televisore() {
+    public Televisore(int codice, String nome, String marca, BigDecimal prezzo, BigDecimal iva, float larghezza, float lunghezza, boolean isSmartTv) {
+        super(codice, nome, marca, prezzo, iva);
+        this.larghezza = larghezza;
+        this.lunghezza = lunghezza;
+        this.isSmartTv = isSmartTv;
+    }
 
+    // setter e getter
+    public void setLarghezza(float larghezza) {
+        this.larghezza = larghezza;
+    }
+    
+    public float getLarghezza() {
+        return this.larghezza;
+    }
+    
+    public void setLunghezza(float lunghezza) {
+        this.lunghezza = lunghezza;
+    }
+    
+    public float getLunghezza() {
+        return this.lunghezza;
+    }
+    
+    public void setIsSmartTv(boolean isSmartTv) {
+        this.isSmartTv = isSmartTv;
+    }
+    
+    public boolean getIsSmartTv() {
+        return this.isSmartTv;
     }
 }
